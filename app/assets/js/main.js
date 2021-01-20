523d7dc348d40a2d47bbac996de701a2f2b21092
Daniil1402 <yadanya14@gmail.com>
2021-01-20 11:40:32 +0300

    validation

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,15 +1,57 @@
-const addTask = document.querySelector(".addTask");
-const createButton = addTask.querySelector("#createButton");
-const taskName = addTask.querySelector("#taskName");
-const newTask = document.querySelector(".tasks");
+(function () {
+  const addTask = document.querySelector(".addTask");
+  const createButton = addTask.querySelector("#createButton");
+  const taskName = addTask.querySelector("#taskName");
+  const newTask = document.querySelector(".tasks");
 
-createButton.addEventListener("click", function () {
-  if (taskName.value) {
-    const task = document.createElement("div");
-    task.classList.add("tasks__item");
-    task.textContent = `${taskName.value}`;
-    newTask.appendChild(task);
+  let tasks = [];
+
+  const renderTasks = function () {
+    for (let item of tasks) {
+      const task = document.createElement("div");
+      task.classList.add("tasks__item");
+      task.textContent = `${item.title}`;
+      newTask.appendChild(task);
+    }
+  };
+
+  if (localStorage.getItem("task") !== null) {
+    tasks = JSON.parse(localStorage.getItem("task"));
+    renderTasks();
+    console.log(tasks);
   } else {
-    alert("Требуется ввести название задачи!");
+    console.log("no");
   }
-});
+
+  const saveInfo = function (obj) {
+    tasks.push(obj);
+    localStorage.setItem("task", JSON.stringify(tasks));
+  };
+
+  const createObj = function (title) {
+    const obj = {
+      title: title,
+      status: 0,
+      id: Date.now(),
+    };
+    saveInfo(obj);
+    return obj;
+  };
+
+  const createNewTask = function () {
+    if (taskName.value) {
+      let taskItem = createObj(taskName.value);
+      const task = document.createElement("div");
+      task.classList.add("tasks__item");
+      task.textContent = `${taskItem.title}`;
+      newTask.appendChild(task);
+      taskName.value = "";
+    } else {
+      alert("Требуется ввести название задачи!");
+    }
+  };
+
+  createButton.addEventListener("click", function () {
+    createNewTask();
+  });
+})();
---
+++ app/assets/js/validation.js
@@ -0,0 +1,23 @@
+(function () {
+  const TASK_TITLE_MAX_LENGTH = 50;
+  const TASK_TITLE_MIN_LENGTH = 3;
+
+  const addTask = document.querySelector(".addTask");
+  const createButton = addTask.querySelector("#createButton");
+  const taskName = addTask.querySelector("#taskName");
+
+  taskName.addEventListener("input", function () {
+    console.log(taskName.value.length);
+    if (taskName.value.length < TASK_TITLE_MIN_LENGTH) {
+      taskName.setCustomValidity(`Название задачи должно быть больше ${TASK_TITLE_MIN_LENGTH} символов. Сейчас количество символов ${taskName.value.length}.`);
+      createButton.disabled = true;
+    } else if (taskName.value.length > TASK_TITLE_MAX_LENGTH) {
+      taskName.setCustomValidity(`Название задачи не должно быть больше ${TASK_TITLE_MAX_LENGTH} символов. Сейчас количество символов ${taskName.value.length}.`);
+      createButton.disabled = true;
+    } else {
+      taskName.setCustomValidity(``);
+      createButton.disabled = false;
+    }
+    taskName.reportValidity();
+  });
+})();
